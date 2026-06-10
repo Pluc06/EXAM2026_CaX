00cb08d190a76003e9d4bb389ecfad5ae814e91b
Phan Van Luc <pvanluc11012k6@gmail.com>
2026-06-10 08:42:32 +0700

    CaX: Thêm component Header

---
+++ reactjs/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className="bg-primary text-white p-4 rounded-3 mb-4">
+            <h2 className="fw-bold">Quản lý danh sách sản phẩm</h2>
+            <p className="mb-0">Giao diện nhập liệu và hiển thị danh sách sản phẩm một cách trực quan, rõ ràng.</p>
+        </div>
+    );
+}
+
+export default Header
